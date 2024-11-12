dda493ba667cd7bc84fbdee146b20d8f97785d6e
me03052008 <161397953+me03052008@users.noreply.github.com>
2024-11-11 21:41:25 -0300

    02

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -1,26 +1,4 @@
-criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
 
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
-
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz OI em inglês?',
-    'Oi em ingles é HI (RAI)'
-) 
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
@@ -39,4 +17,3 @@ function criaCartao(categoria, pergunta, resposta) {
     `
     container.appendChild(cartao)
 
-}
